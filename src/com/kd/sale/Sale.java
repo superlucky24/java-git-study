690b0c41690ac5044b7024dc00f6a1a27134fb97
superlucky24 <ghparc24@gmail.com>
2026-01-12 14:33:22 +0900

    세일추가

---
+++ src/com/kd/sale/Sale.java
@@ -0,0 +1,10 @@
+package com.kd.sale;
+
+import com.kd.market.dto.Product;
+
+public class Sale {
+	Product product = new Product();
+	public Product getSaleProduct() {
+		return product;
+	}
+}
---
+++ src/com/kd/sale/SaleTest.java
@@ -0,0 +1,11 @@
+package com.kd.sale;
+
+public class SaleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sale sale = new Sale();
+		System.out.println(sale.getSaleProduct());
+	}
+
+}
